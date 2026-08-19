56b8e7acb80a1c7fcb44581de62dcc3567df801c
brweinstein <brweinstein68@gmail.com>
2026-08-19 19:44:31 -0400

    updated main.py with CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import prices, recommendation
 
 app = FastAPI(title="When Should I Use Electricity?")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 app.include_router(prices.router)
 app.include_router(recommendation.router)
